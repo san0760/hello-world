84413709d1822c8c18a3bfcf7ee82c4c8e1f1bcb
san0760 <net0760@hotmail.com>
2016-04-24 13:45:34 +0800

    Added files via upload

--- threaddemo.java
+++ threaddemo.java
@@ -1,26 +1,100 @@
-class demo implements Runnable 
-{	
-	public void run() 
+package thread;
+import java.util.concurrent.locks.*;	
+
+
+class resource
+{
+	private int count=0;
+	private boolean flag=false;
+	final Lock lock=new ReentrantLock();
+	final Condition con1=lock.newCondition();
+	final Condition con2=lock.newCondition();
+	public void product() throws InterruptedException
 	{
-		int i=1;				
-		System.out.println(Thread.currentThread().getName()+"......."+i);
-		
+		lock.lock();
+		try
+		{
+			while(flag)
+			{
+				con1.await();
+			}
+			count++;
+			System.out.println("food-product"+count);
+			flag=true;
+			con2.signal();
+		}
+		finally
+		{
+			lock.unlock();
+		}
+	}
+	public void eat() throws InterruptedException
+	{
+		lock.lock();
+		try
+		{
+			while(!flag)
+			{
+				con2.await();
+			}
+			System.out.println("food-eat"+count);
+			flag=false;
+			con1.signal();
+		}
+		finally
+		{
+			lock.unlock();
+		}
 	}
 }
 
-class threaddemo 
+class producter implements Runnable
+{
+	private resource r;
+	producter(resource r)
+	{
+		this.r=r;
+	}
+	public void run()
+	{
+		while(true)
+		{
+			try{r.product();}catch(InterruptedException e){}
+		}
+	}
+}
+
+class custmer implements Runnable
+{
+	private resource r;
+	custmer(resource r)
+	{
+		this.r=r;
+	}
+	public void run()
+	{
+		while(true)
+		{
+			try{r.eat();}catch(InterruptedException e){}
+		}
+	}
+}
+
+class  prducterdemo
 {
 	public static void main(String[] args) 
 	{
-		System.out.println("---This is a demo test---");
-		demo d = new demo();
-		Thread a = new Thread(d);
-		a.setName("haha");
-		Thread b = new Thread(d);
-		b.setName("hehe");
-		a.start();
-		b.start();
-		
-		
+		resource r=new resource();
+		producter p=new producter(r);
+		custmer c=new custmer(r);
+		Thread t1=new Thread(p);
+		Thread t2=new Thread(p);
+		Thread t3=new Thread(c);
+		Thread t4=new Thread(c);
+		t1.start();
+		t2.start();
+		t3.start();
+		t4.start();
 	}
 }
+
