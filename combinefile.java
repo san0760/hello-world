ac2d36a55db7fc9f2e3562f3dc103824adb2d05d
san0760 <net0760@hotmail.com>
2016-05-19 21:44:05 +0800

    Update combinefile.java

--- combinefile.java
+++ combinefile.java
@@ -5,6 +5,7 @@ import java.io.SequenceInputStream;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Enumeration;
+import java.util.Properties;
 
 public class combinefile
 {
@@ -12,7 +13,12 @@ public class combinefile
 	public static void main(String[] args) throws IOException
 	{
 		ArrayList<FileInputStream> al = new ArrayList<FileInputStream>();
-		for(int i=0;i<=3;i++)
+		Properties prop=new Properties();
+		FileInputStream fiss=new FileInputStream("F:\\JAVA\\Learn\\IO\\0.Properties");
+		prop.load(fiss);
+		String filename=prop.getProperty("filename");
+		int a=Integer.parseInt(prop.getProperty("count"));
+		for(int i=0;i<=a;i++)
 		{
 			al.add(new FileInputStream(i+".part"));
 		}
@@ -20,7 +26,7 @@ public class combinefile
 		SequenceInputStream sis=new SequenceInputStream(en);
 		byte[] buf=new byte[1024*1024];
 		int len=0;
-		FileOutputStream fos=new FileOutputStream("1.bmp");
+		FileOutputStream fos=new FileOutputStream(filename);
 		while((len=sis.read(buf))!=-1)
 		{
 			fos.write(buf, 0, len);
